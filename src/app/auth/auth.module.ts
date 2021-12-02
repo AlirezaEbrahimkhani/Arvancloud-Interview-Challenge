35796d2ddf8e3df8bcef8671321f95ba760ee7bd
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2021-12-03 01:32:50 +0330

    [Feat] declare login and register component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AuthRoutingComponent } from './auth-routing.component';
 import { AuthRoutingModule } from './auth.routing';
+import { LoginComponent, RegisterComponent } from './components';
 import { UserService } from './shared';
 
 @NgModule({
-  declarations: [AuthRoutingComponent],
+  declarations: [AuthRoutingComponent, LoginComponent, RegisterComponent],
   imports: [
     // angular
     CommonModule,
