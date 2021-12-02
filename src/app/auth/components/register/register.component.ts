15a29d0694e3456e69921e748cca21a0e7ef40a0
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2021-12-03 03:08:34 +0330

    [Feat] connect form to userService and create register form

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '@app/auth/shared';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+  form: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private readonly _fb: FormBuilder,
+    private readonly _userService: UserService
+  ) {
+    this.form = this._fb.group({
+      username: [null, Validators.required],
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, Validators.required],
+    });
   }
 
+  onSubmit() {
+    if (this.form.valid) this._userService.register(this.form.value);
+  }
 }
