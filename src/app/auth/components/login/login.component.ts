c4de6b5aa70769ca8e00e6b85e0b58aebf0e1616
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2021-12-03 02:58:38 +0330

    [Feat] connect form to userService and create login form

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+// angular
+
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '@app/auth/shared';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  form: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private readonly _fb: FormBuilder,
+    private readonly _userService: UserService
+  ) {
+    this.form = this._fb.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, Validators.required],
+    });
   }
 
+  onSubmit() {
+    if (this.form.valid) this._userService.login(this.form.value);
+  }
 }
