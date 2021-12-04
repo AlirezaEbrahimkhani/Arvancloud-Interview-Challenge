fda893eccc803abc277615ba8f170574b1d89375
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2021-12-05 01:05:15 +0330

    [Feat] logic for render articleList and dropdown behaviour

--- src/app/article/components/article-list/article-list.component.ts
+++ src/app/article/components/article-list/article-list.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
+import { ArticleService } from '@app/article/shared';
+import { Article } from '@app/article/shared/interfaces';
+import { Toaster } from '@shared/toast-notification';
 
 @Component({
   selector: 'app-article-list',
   templateUrl: './article-list.component.html',
-  styleUrls: ['./article-list.component.scss']
+  styleUrls: ['./article-list.component.scss'],
 })
-export class ArticleListComponent implements OnInit {
+export class ArticleListComponent {
+  constructor(
+    public readonly articleService: ArticleService,
+    private readonly _renderer: Renderer2,
+    private readonly _toaster: Toaster
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  toggleDropdown(dropdown) {
+    if (dropdown) {
+      if (dropdown.classList.value.includes('show')) {
+        this._renderer.removeClass(dropdown, 'show');
+      } else {
+        this._renderer.addClass(dropdown, 'show');
+      }
+    }
   }
 
+  onDelete(article: Article) {}
 }
