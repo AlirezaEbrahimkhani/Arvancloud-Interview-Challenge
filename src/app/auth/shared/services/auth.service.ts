c1d05b33a0d0ccfc9bdac95a851aab9f9e8a3585
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2021-12-04 13:21:21 +0330

    [Feat] handle login and register error

--- src/app/auth/shared/services/auth.service.ts
+++ src/app/auth/shared/services/auth.service.ts
@@ -1,15 +1,22 @@
 // angular
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 
 // 3rd party
-import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+import {
+  BehaviorSubject,
+  EMPTY,
+  Observable,
+  Subscription,
+} from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 // app
 import { SubscriptionManager } from '@app/core';
 import { environment } from '@env/environment';
 import { LoginModel, RegisterModel, User } from '../interfaces';
+import { Toaster } from '@shared/toast-notification';
 
 @Injectable()
 export class AuthService extends SubscriptionManager {
@@ -22,16 +29,16 @@ export class AuthService extends SubscriptionManager {
 
   constructor(
     private readonly _router: Router,
-    private readonly _httpClient: HttpClient
+    private readonly _httpClient: HttpClient,
+    private readonly _toaster: Toaster
   ) {
     super();
   }
 
-  // TODO: Implement Error handling ...
-
-  login(loginModel: LoginModel) {
+  public login(loginModel: LoginModel) {
     let loginSubscription: Subscription = this._httpClient
       .post<{ user: User }>(`${this._baseUrl}users/login`, { user: loginModel })
+      .pipe(catchError(this._handleLoginError))
       .subscribe(({ user }) => {
         this._currentUser.next(user);
         localStorage.setItem('token', user.token);
@@ -40,18 +47,40 @@ export class AuthService extends SubscriptionManager {
     this.addSubscription$('login', loginSubscription);
   }
 
-  register(registerModel: RegisterModel) {
+  public register(registerModel: RegisterModel) {
     let registerSubscription: Subscription = this._httpClient
       .post(`${this._baseUrl}users`, { user: registerModel })
+      .pipe(catchError(this._handleRegistrationError))
       .subscribe(() => {
         this._router.navigate(['/login']);
       });
     this.addSubscription$('register', registerSubscription);
   }
 
-  logout() {
+  public logout() {
     this._currentUser.next(null);
     localStorage.removeItem('token');
     this._router.navigate(['/login']);
+  }
+
+  private _handleLoginError(error: HttpErrorResponse) {
+    if (error) {
+      this._toaster.open({
+        type: 'danger',
+        caption: 'Login failed',
+        text: 'Username and/or Password is invalid!',
+      });
+    }
+    return EMPTY;
+  }
+
+  private _handleRegistrationError(error: HttpErrorResponse) {
+    if (error) {
+      this._toaster.open({
+        type: 'danger',
+        text: 'Registration Failed !',
+      });
+    }
+    return EMPTY;
   }
 }
