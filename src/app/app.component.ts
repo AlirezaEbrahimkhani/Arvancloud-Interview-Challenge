fad5ac66b4a2e0bbfffd23ce0aa2f0dc5398be51
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2021-12-05 01:00:32 +0330

    [Feat] add spinner for resolvers loading status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
-
+import { merge, Observable } from "rxjs";
+import { ResolveEnd, ResolveStart, Router } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
+import { filter, mapTo } from "rxjs/operators";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  isLoading$!: Observable<boolean>;
+  private _showLoaderEvents$!: Observable<boolean>;
+  private _hideLoaderEvents$!: Observable<boolean>;
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this._showLoaderEvents$ = this.router.events.pipe(
+      filter((e) => e instanceof ResolveStart),
+      mapTo(true)
+    );
+    this._hideLoaderEvents$ = this.router.events.pipe(
+      filter((e) => e instanceof ResolveEnd),
+      mapTo(false)
+    );
+    this.isLoading$ = merge(this._hideLoaderEvents$, this._showLoaderEvents$);
+  }
+}
