770790c11d27c698702310d2a5c0ac81f8ae6ed7
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2021-12-05 13:11:59 +0330

    [Feat] implement article form template and basic state of the form

--- src/app/article/components/article-form/article-form.component.ts
+++ src/app/article/components/article-form/article-form.component.ts
@@ -1,15 +1,68 @@
+// angular
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+
+// app
+import { ArticleService } from '@app/article/shared';
+import { SubscriptionManager } from '@app/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-article-form',
   templateUrl: './article-form.component.html',
-  styleUrls: ['./article-form.component.scss']
+  styleUrls: ['./article-form.component.scss'],
 })
-export class ArticleFormComponent implements OnInit {
+export class ArticleFormComponent
+  extends SubscriptionManager
+  implements OnInit
+{
+  form: FormGroup;
+  tags: string[] = [];
+  loading: boolean = false;
+  isEditMode: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private readonly _formBuilder: FormBuilder,
+    private readonly _route: ActivatedRoute,
+    private readonly _articleService: ArticleService
+  ) {
+    super();
+    this.form = this._formBuilder.group({
+      title: [null, Validators.required],
+      description: [null, Validators.required],
+      body: [null, Validators.required],
+    });
   }
 
+  ngOnInit(): void {
+    this._articleService.getTagList();
+    this.isEditMode = this._route.snapshot.url[0].path === 'edit';
+    if (this.isEditMode) this._getEditArticleData();
+    else this._getTagList();
+  }
+
+  private _getEditArticleData() {
+    let articleDataSubscription: Subscription = this._route.data.subscribe(
+      ({ article }) => this._fillDataIntoForm(article.article)
+    );
+    this.addSubscription$('editArticleData', articleDataSubscription);
+  }
+
+  private _fillDataIntoForm(articleData) {
+    this.form.setValue({
+      title: articleData.title,
+      description: articleData.description,
+      body: articleData.body,
+    });
+  }
+
+  private _getTagList() {
+    let tagListSubscription: Subscription = this._route.data.subscribe(
+      ({ tags }) => (this.tags = tags)
+    );
+    this.addSubscription$('tagList', tagListSubscription);
+  }
+
+  onArticleSubmit() {}
 }
