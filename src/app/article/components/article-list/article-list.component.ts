c48de8a665ae0aea4e466d67d8abfbf92fa29b34
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2021-12-05 12:29:22 +0330

    [Feat] handle subscription with subscriptionManager class

--- src/app/article/components/article-list/article-list.component.ts
+++ src/app/article/components/article-list/article-list.component.ts
@@ -1,19 +1,27 @@
+// angular
 import { Component, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { take } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+
+// app
 import { ArticleService } from '@app/article/shared';
 import { Article } from '@app/article/shared/interfaces';
+import { SubscriptionManager } from '@app/core';
 import {
   ConfirmationDialogComponent,
   ConfirmationDialogService,
 } from '@shared/confirmation-dialog';
 import { Toaster } from '@shared/toast-notification';
-import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-article-list',
   templateUrl: './article-list.component.html',
   styleUrls: ['./article-list.component.scss'],
 })
-export class ArticleListComponent implements OnInit {
+export class ArticleListComponent
+  extends SubscriptionManager
+  implements OnInit
+{
   @ViewChild('confirmationDialog', { static: true })
   confirmationDialog: ConfirmationDialogComponent;
   private _dropdown;
@@ -23,7 +31,9 @@ export class ArticleListComponent implements OnInit {
     private readonly _renderer: Renderer2,
     private readonly _toaster: Toaster,
     private _confirmationDialogService: ConfirmationDialogService
-  ) {}
+  ) {
+    super();
+  }
 
   ngOnInit(): void {
     this._confirmationDialogService.register(this.confirmationDialog);
@@ -42,11 +52,11 @@ export class ArticleListComponent implements OnInit {
 
   onDelete(article: Article) {
     this._confirmationDialogService.showDialog();
-    this._confirmationDialogService.result
+    let dialogResult: Subscription = this._confirmationDialogService.result
       .pipe(take(1))
       .subscribe((res: boolean) => {
         this._renderer.removeClass(this._dropdown, 'show');
-        console.log(res);
       });
+    this.addSubscription$('dialogResult', dialogResult);
   }
 }
