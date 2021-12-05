fca5a54e19574e91030fc521982c6a0cd15cb029
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2021-12-06 00:16:20 +0330

    [Feat] update and create article functionality

--- src/app/article/components/article-form/article-form.component.ts
+++ src/app/article/components/article-form/article-form.component.ts
@@ -1,12 +1,15 @@
 // angular
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription, throwError } from 'rxjs';
 
 // app
 import { ArticleService } from '@app/article/shared';
+import { CreateArticleDTO } from '@app/article/shared/interfaces';
 import { SubscriptionManager } from '@app/core';
-import { Subscription } from 'rxjs';
+import { Toaster } from '@shared/toast-notification';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-article-form',
@@ -26,13 +29,15 @@ export class ArticleFormComponent
   constructor(
     private readonly _formBuilder: FormBuilder,
     private readonly _route: ActivatedRoute,
-    private readonly _articleService: ArticleService
+    private readonly _articleService: ArticleService,
+    private readonly _toaster: Toaster,
+    private readonly _router: Router
   ) {
     super();
     this.form = this._formBuilder.group({
       title: [null, Validators.required],
-      description: [null, Validators.required],
-      body: [null, Validators.required],
+      description: [null],
+      body: [null],
     });
 
     this.tagListForm = this._formBuilder.group({
@@ -47,6 +52,17 @@ export class ArticleFormComponent
     else this._getTagList();
   }
 
+  onArticleSubmit() {
+    if (this.isEditMode) this._updateArticle(this.form.value);
+    else this._createArticle(this.form.value);
+  }
+
+  onSubmitTag() {
+    const { tagName } = this.tagListForm.value;
+    if (tagName) this.tags.push(tagName);
+    this.tagListForm.reset();
+  }
+
   private _getEditArticleData() {
     let articleDataSubscription: Subscription = this._route.data.subscribe(
       ({ article }) => this._fillDataIntoForm(article.article)
@@ -55,25 +71,72 @@ export class ArticleFormComponent
   }
 
   private _fillDataIntoForm(articleData) {
+    const { title, description, body, tagList } = articleData;
+    this.tags = tagList.sort() ?? [];
     this.form.setValue({
-      title: articleData.title,
-      description: articleData.description,
-      body: articleData.body,
+      title,
+      description,
+      body,
     });
   }
 
   private _getTagList() {
     let tagListSubscription: Subscription = this._route.data.subscribe(
-      ({ tags }) => (this.tags = tags)
+      ({ tags }) => (this.tags = tags.sort())
     );
     this.addSubscription$('tagList', tagListSubscription);
   }
 
-  onArticleSubmit() {}
+  private _updateArticle(formValue: CreateArticleDTO) {
+    let body = { ...formValue, tagList: this.tags };
+    const slug = this._route.snapshot.paramMap.get('slug');
+    this._articleService
+      .updateArticle(slug, body)
+      .pipe(
+        catchError((error) => {
+          this._toaster.open({
+            type: 'danger',
+            caption: 'Error ...',
+            text: 'An error occurred while updating article !',
+          });
+          return throwError(error);
+        })
+      )
+      .subscribe((response) => {
+        if (response) {
+          this._toaster.open({
+            type: 'success',
+            caption: 'Well done!',
+            text: 'Article updated successfully !',
+          });
+          this._router.navigate(['/articles']);
+        }
+      });
+  }
 
-  onSubmitTag() {
-    const { tagName } = this.tagListForm.value;
-    this.tags.push(tagName);
-    this.tagListForm.reset();
+  private _createArticle(formValue: CreateArticleDTO) {
+    let body = { ...formValue, tagList: this.tags };
+    this._articleService
+      .createArticle(body)
+      .pipe(
+        catchError((error) => {
+          this._toaster.open({
+            type: 'danger',
+            caption: 'Error ...',
+            text: 'An error occurred while creating article !',
+          });
+          return throwError(error);
+        })
+      )
+      .subscribe((response) => {
+        if (response) {
+          this._toaster.open({
+            type: 'success',
+            caption: 'Well done!',
+            text: 'Article created successfully !',
+          });
+          this._router.navigate(['/articles']);
+        }
+      });
   }
 }
